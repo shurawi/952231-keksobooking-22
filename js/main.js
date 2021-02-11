462959f2d3a1331755c2411cf5d1272c4085cf14
shurawi <denis.b.toropov@gmail.com>
2021-02-11 20:52:52 +0700

    исправляет main.js

--- js/main.js
+++ js/main.js
@@ -5,20 +5,24 @@ let getRandomInt = function (min, max) {
   if (min < 0 || max < 0 || min > max) {
     return alert('Введенные числа должны быть положительными. Минимальное значение диапазона не должно превышать максимальное.');
   } else if (min == max) {
-      return max;
+    return max;
   } else {
-      return Math.floor(Math.random() * (max - min + 1)) + min;
+    return Math.floor(Math.random() * (max - min + 1)) + min;
   }
 }
+
+getRandomInt(2, 15);
 
 let getRandomIntDot = function (min, max, dot) {
   if (min < 0 || max < 0 || min > max || dot <= 0 ) {
     return alert('Введенные числа должны быть положительными. Минимальное значение диапазона не должно превышать максимальное.');
   } else if (min == max) {
-      return max;
+    return max;
   } else {
     let int = Math.random() * (max - min) + min;
     let intDot = int.toFixed(dot);
     return intDot;
   }
 }
+
+getRandomIntDot(3, 7, 4);
